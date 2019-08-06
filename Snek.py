c84bd5825254aa375142c1681afceb6c213a34a6
Amina <amshikhalieva@gmail.com>
2019-08-05 23:51:55 -0500

    Fixed attemptMove boundary check bugs

--- Snek.py
+++ Snek.py
@@ -2,21 +2,19 @@ from tkinter import *
 from tkinter import ttk
 
 class segment():
-    
     def __init__(self, can, x, y, dist, prev = None):
         self.x = x
         self.y = y
         self.dist = dist
         self.can = can
         self.prev = prev
-        square = can.create_rectangle(x-dist, y-dist, x+dist, y+dist)
-        can.itemconfigure(square, fill = 'dark green')
+        self.square = can.create_rectangle(x-dist, y-dist, x+dist, y+dist)
+        can.itemconfigure(self.square, fill = 'dark green')
     def setPrev(self, node):
         self.prev = node
-    def deleteSeg(self):
-        canvas.delete(self.square)
-
-        
+    def deleteSeg(self, can):
+        can.delete(self.square)
+   
 class Snek ():
     x = 320
     y = 420
@@ -25,24 +23,19 @@ class Snek ():
     direction = "up"
     segSet = {(x, y)}
     def __init__(self, canvas):
-        self.canvas = canvas
+        Snek.canvas = canvas
         canvas.config(width = 640, height = 480)
-
-
-
-
-        
-        self.head = segment(canvas, Snek.x, Snek.y, Snek.segSize/2)                                 #fix order, add these to segSet
-        self.tail = segment(canvas, Snek.x, Snek.y+2*Snek.segSize, Snek.segSize/2,
-                            segment(canvas, Snek.x, Snek.y+Snek.segSize, Snek.segSize/2, self.head))
-        segSet.add((Snek.x, Snek.y),(
-
-
+        Snek.head = segment(canvas, Snek.x, Snek.y, Snek.segSize/2)                                 
+        Snek.tail = segment(canvas, Snek.x, Snek.y+2*Snek.segSize, Snek.segSize/2,
+                            segment(canvas, Snek.x, Snek.y+Snek.segSize, Snek.segSize/2, Snek.head))
+        Snek.segSet.add((Snek.x, Snek.y))
+        Snek.segSet.add((Snek.x, Snek.y+2*Snek.segSize))
+        Snek.segSet.add((Snek.x, Snek.y+Snek.segSize))
+        #Snek.y = Snek.y-2*Snek.segSize
+            
         
 ##        seg2 = segment(canvas, Snek.x, Snek.y+Snek.segSize, Snek.segSize/2)
-##        self.tail = segment(canvas, Snek.x, Snek.y+2*Snek.segSize, Snek.segSize/2)
-        
-
+##        Snek.tail = segment(canvas, Snek.x, Snek.y+2*Snek.segSize, Snek.segSize/2)     
 ##    def turn (way):
 ##        switch currentLocation{
 ##            case "w":
@@ -61,36 +54,38 @@ class Snek ():
 ##                if (curentDirection.equals("up")|| currentDirection.equals("down")):
 ##                    currentDirection = "right"
 ##                break
-##        }
-            
-    
+##        }          
     def attemptMove (self):
-        #dist =  self.segSize/2
-        if (self.x<self.segSize or self.x>640-self.segSize or self.y<self.segSize or self.y>480-self.segSize):
-            return False
-        #create new node in current direction
-        if (self.direction == 'up'):
-            self.y += self.segSize
-        elif (self.direction == 'down'):
-            self.y -= self.segSize
-        elif (self.direction == 'left'):
-            self.x -= self.segSize
+        if (Snek.direction == 'up'):
+            Snek.y -= Snek.segSize     
+        elif (Snek.direction == 'down'):
+            Snek.y += Snek.segSize
+        elif (Snek.direction == 'left'):
+            Snek.x -= Snek.segSize
         else:
-            self.x += self.segSize
-                                                                                                             
-        print ('in attemptmove(), new x is ', self.x, 'new y is ', self.y, '\n')                     #complicated bc centers of segnemts don't perfectly overlap
-        if ((self.x, self.y) in self.segSet):
+            Snek.x += Snek.segSize
+            
+        print ('in attemptmove(), new x is ', Snek.x, 'new y is ', Snek.y, '\n')                     #complicated bc centers of segnemts don't perfectly overlap
+        print('segSet:')
+        for i in Snek.segSet:
+            print(i)
+            
+        if (Snek.x<Snek.segSize or Snek.x>640-Snek.segSize or Snek.y<Snek.segSize or Snek.y>480-Snek.segSize):
+            print('returning false bc boundary check')
             return False
-        
-        self.segSet.add((self.x, self.y)) 
-        new = segment(self.canvas, self.x, self.y, self.segSize/2)
-        #self.tail.setNext(new)                                                       #why need this
-        
-        self.tail = new
-        
+        if ((Snek.x, Snek.y) in Snek.segSet):
+            print('returning false because x,y in segSet')
+            return False
+        Snek.segSet.add((Snek.x, Snek.y))              
+        new = segment(Snek.canvas, Snek.x, Snek.y, Snek.segSize/2)
+        Snek.tail.deleteSeg(Snek.canvas)
+        Snek.segSet.discard((Snek.x, Snek.y))    
+        Snek.tail = new
         return True
         
 ##    def eat ():
 ##        #undraw dot
-##        #increment body by 1 square
+
+
+
     
